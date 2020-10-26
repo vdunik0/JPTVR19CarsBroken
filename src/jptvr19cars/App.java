2604745f4cdab783ba81c8eb4783549236d60eff
pupil <pupil@ivkhk.ee>
2020-10-26 11:15:35 +0200

    Загттовка проекта nomer 2

--- src/jptvr19cars/App.java
+++ src/jptvr19cars/App.java
@@ -5,14 +5,31 @@
  */
 package jptvr19cars;
 
+import entity.Buyeer;
+import entity.Cars;
+import entity.History;
+        
 /**
  *
  * @author pupil
  */
 class App {
 
+    public App() {
+        Buyeer buyeer = new Buyeer();
+        Cars cars = new Cars();
+        History history = new History();
+    }
+
     public void run() {
-       
+        /**
+        * 1.Добавить товар
+        * 2.Список товаров
+        * 3.Добавить покупателя
+        * 4.Список покупателя
+        * 5.Покупателю купить товар(при покупке товара из кошелька покупателя вычитать стоимость)
+        **/
+        
     }
     
 }
